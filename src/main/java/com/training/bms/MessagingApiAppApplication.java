a0ab40da11d73dbb44705720dc3a3753d07b80ba
nirajgupta6 <nirajgupta0605@gmail.com>
2024-07-12 14:23:24 +0530

    first commit

---
+++ src/main/java/com/training/bms/MessagingApiAppApplication.java
@@ -0,0 +1,17 @@
+package com.training.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingApiAppApplication {
+	
+	public void printGreetings() {
+		System.out.println("Hi Oracle");
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingApiAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/bms/MessagingApiAppApplicationTests.java
@@ -0,0 +1,18 @@
+package com.training.bms;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+class  AlstromAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+		assertEquals (1990,190);
+	}
+
+}
